6e799f0398f0be553d72a2c331477c1ff29d42ac
Jony Jaime <jonatan.jaime8@gmail.com>
2025-11-29 16:40:15 -0300

    feat: :sparkles: dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,28 @@
 import AuthGuard from "@/components/shared/auth-guard";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import Script from "next/script";
 import { Toaster } from "react-hot-toast";
+
+import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header"
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGuard>
-      {children}
+      <SidebarProvider>
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                {children}
+              </div>
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+
       <Toaster position="top-center" />
       {process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT && <Script async src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT}`} />}
     </AuthGuard>
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,112 @@
+"use client"
+
+import {
+  Building,
+  FolderIcon,
+  HelpCircleIcon,
+  LayoutDashboardIcon,
+  SearchIcon,
+  SettingsIcon,
+  UsersIcon
+} from "lucide-react"
+import * as React from "react"
+
+import { NavAdmin } from "@/components/nav-admin"
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Logo from "./shared/logo"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+      icon: LayoutDashboardIcon,
+    },
+    {
+      title: "Desarrollos",
+      url: "/desarrollos",
+      icon: FolderIcon,
+    },
+    {
+      title: "Empleados",
+      url: "/empleados",
+      icon: UsersIcon,
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: SettingsIcon,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: HelpCircleIcon,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: SearchIcon,
+    },
+  ],
+  admin: [
+    {
+      name: "Usuarios",
+      url: "#",
+      icon: UsersIcon,
+    },
+    {
+      name: "Construcciones",
+      url: "#",
+      icon: Building,
+    },
+
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <div>
+                <a href="#" className="w-28">
+                  <Logo />
+                </a>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavAdmin items={data.admin} />
+        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
